53a69fb7be854d99deaa8a6aa952b5d47ea2ed21
Chetan Tekam <tekamchetan3500@gmail.com>
2025-06-26 17:21:20 +0000

    insert_data.sh modified.

--- insert_data.sh
+++ insert_data.sh
@@ -23,29 +23,16 @@ do
     if [[ -z $TEAM_WIN_ID ]]
     then
       INSERT_WINNER_TEAMS=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      TEAM_WIN_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
     fi
     # get opponent team id
     TEAM_OPP_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
     if [[ -z $TEAM_OPP_ID ]]
     then
       INSERT_OPPONENT_TEAMS=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      TEAM_OPP_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
     fi
-  fi
-done
-
-cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
-do
-  if [[ $YEAR != year ]]
-  then  
-    TEAM_WIN_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-    TEAM_OPP_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
     SQL="INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $TEAM_WIN_ID, $TEAM_OPP_ID, $WINNER_GOALS, $OPPONENT_GOALS);"
     RESULT=$($PSQL "$SQL")
-    if [[ $RESULT == "INSERT 0 1" ]]
-    then
-      echo $YEAR, $ROUND, $TEAM_WIN_ID, $TEAM_OPP_ID, $WINNER_GOALS, $OPPONENT_GOALS
-    else
-      echo ERR
-    fi
   fi
 done
